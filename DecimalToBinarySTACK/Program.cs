d0efc6274bd668dc9edc8c5f6cb56846857586c9
Afnan Hassan 2.0 <afnanirshad4070@gmail.com>
2024-06-11 14:52:33 +0500

    initial commit

---
+++ DecimalToBinarySTACK/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalToBinarySTACK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Decimal Number : ");
+            int decimalNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+        }
+    }
+}
---
+++ EvenOddChecker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOddChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number : ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            if (userInput % 2 == 0) { Console.WriteLine($"The Given Number {userInput} is an EVEN"); }
+            else Console.WriteLine($"The Given Number {userInput} is an ODD");
+            Console.ReadLine();
+        }
+    }
+}
