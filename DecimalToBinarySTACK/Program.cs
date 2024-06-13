68d04757ba3806caea7def5627920fbb83be821f
Afnan Hassan 2.0 <afnanirshad4070@gmail.com>
2024-06-13 22:46:08 +0500

    null change

--- DecimalToBinarySTACK/Program.cs
+++ DecimalToBinarySTACK/Program.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿﻿
 namespace DecimalToBinarySTACK
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the Decimal Number : ");
             int decimalNumber = Convert.ToInt32(Console.ReadLine());
+            List<int> stack = new List<int>(); // to store the obtained digit in binary
+            while (decimalNumber > 0) // calculation of binary & decimal
+            {
+                stack.add(decimalNumber % 2);
+                decimalNumber /= 2; // next decimal input
+            }
+            for (int i = stack.Count; i > -1; i--)
+            {
+                Console.Write(stack[i]);
+            }
             Console.WriteLine();
         }
     }
