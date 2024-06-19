743fa1d48981b947ce5274584342438de659e489
Afnan Hassan 2.0 <afnanirshad4070@gmail.com>
2024-06-19 22:03:18 +0500

    new logic

--- DecimalToBinarySTACK/Program.cs
+++ DecimalToBinarySTACK/Program.cs
@@ -1,23 +1,42 @@
-﻿﻿
+﻿using System;
+using System.Collections.Generic;
 namespace DecimalToBinarySTACK
 {
     class Program
     {
+        static long userInput; // stores user input on run time
+        static long binaryNumber; // stores found binary number
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Decimal Number : ");
-            int decimalNumber = Convert.ToInt32(Console.ReadLine());
-            List<int> stack = new List<int>(); // to store the obtained digit in binary
-            while (decimalNumber > 0) // calculation of binary & decimal
+            Console.Write("Enter Decimal Number : ");
+            userInput = Convert.ToInt64(Console.ReadLine()); // user input
+            binaryNumber = findBinaryNumber(); // Binary calculations
+            binaryNumber = binaryReverseKaro(binaryNumber); // Reverses the Binary Number
+            Console.WriteLine("The Binary Number : " + binaryNumber);
+
+            Console.ReadLine(); // raw line to keep the terminal
+        }
+        static long findBinaryNumber() // returns the reverse binary number
+        {
+            long decimalNumber = userInput; // temporary number to keep userInput Integrated
+            long reverseBinary = 0;
+            while (decimalNumber >= 0) // Factorization Process
             {
-                stack.add(decimalNumber % 2);
-                decimalNumber /= 2; // next decimal input
+                int digit = Convert.ToInt16(decimalNumber % 2); // gets the last digit
+                reverseBinary = reverseBinary * 10 + digit; // composing the binary number in reverse order
+                decimalNumber = decimalNumber / 10; // moving on to the next factorization stage
             }
-            for (int i = stack.Count; i > -1; i--)
+            return reverseBinary;
+        }
+        static long binaryReverseKaro(long givenNumber) // reversing the number
+        {
+            long finalBinary = 0;
+            while (givenNumber >= 0)
             {
-                Console.Write(stack[i]);
+                finalBinary = (finalBinary * 10) + (givenNumber % 10); // last digit
+                givenNumber = givenNumber / 10; // next number stage
             }
-            Console.WriteLine();
+            return finalBinary;
         }
     }
 }
