9c88b0d8a9c73c06ebbb3a47a42253b6bbd1bff1
Afnan Hassan 2.0 <afnanirshad4070@gmail.com>
2024-06-26 05:25:06 +0500

    conversion to Top Level Statements Paradigm

--- EvenOddChecker/Program.cs
+++ EvenOddChecker/Program.cs
@@ -1,20 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//
+CheckEvenOdd();
 
-namespace EvenOddChecker
+
+void CheckEvenOdd()
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Write("Enter a number : ");
-            int userInput = Convert.ToInt32(Console.ReadLine());
-            if (userInput % 2 == 0) { Console.WriteLine($"The Given Number {userInput} is an EVEN"); }
-            else Console.WriteLine($"The Given Number {userInput} is an ODD");
-            Console.ReadLine();
-        }
-    }
+    Console.Write("Enter a number : ");
+    int userInput = Convert.ToInt32(Console.ReadLine());
+    if (userInput % 2 == 0) { Console.WriteLine($"The Given Number {userInput} is an EVEN"); }
+    else Console.WriteLine($"The Given Number {userInput} is an ODD");
+    Console.ReadLine();
 }
