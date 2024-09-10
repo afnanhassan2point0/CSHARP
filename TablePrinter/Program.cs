86f985781e084f78ee1bffdf41b364828bbe9ba0
Afnan Hassan 2.0 <afnanirshad4070@gmail.com>
2024-09-10 07:21:07 +0500

    starter

--- TablePrinter/Program.cs
+++ TablePrinter/Program.cs
@@ -4,8 +4,6 @@ static class Program
     static void Main()
     {
         ApplicationConfiguration.Initialize();
-        // creating a window
-        Form table_Printer_Window = new Form();
         Application.Run(new Form1());
     }
 }
